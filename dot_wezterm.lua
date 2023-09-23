d01f58f0a11a2003e8fe7e6c5d92e4e40f8ef796
Dan <dancooling89@outlook.com>
2023-09-23 13:29:18 +0100

    Remove titlebar and add window padding to wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -10,4 +10,15 @@ config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 18
 
+-- Configure the title bar
+config.enable_tab_bar = false
+
+-- Configure window padding
+config.window_padding = {
+	left = 20,
+	right = 20,
+	top = 20,
+	bottom = 20,
+}
+
 return config
